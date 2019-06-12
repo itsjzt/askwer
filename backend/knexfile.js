9eac1bb28e004cb8f566f15e5560cbae0fa6bf87
Saurabh Sharma <saurabh.19@outlook.com>
2019-06-12 16:53:18 +0530

    added initial database migration

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,10 +2,10 @@ module.exports = {
   development: {
     client: "pg", // pg is the database library for postgreSQL on knexjs
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_DATABASE
+      host: "127.0.0.1",
+      user: "postgres",
+      password: "root",
+      database: "db_name"
     }
   },
   production: {
---
+++ backend/migrations/20190612160548_add.js
@@ -0,0 +1,50 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable("user", function(t) {
+      t.increments("id")
+        .unsigned()
+        .primary();
+      t.string("name").notNull();
+      t.text("description").nullable();
+      t.string("email").notNull();
+      t.string("password").notNull();
+      t.timestamps();
+    })
+    .createTable("question", function(t) {
+      t.increments("id")
+        .unsigned()
+        .primary();
+      t.string("text").notNull();
+      t.text("decription").nullable();
+      t.integer("author")
+        .references("id")
+        .inTable("user")
+        .notNull()
+        .onDelete("cascade");
+      t.timestamps();
+    })
+    .createTable("answer", function(t) {
+      t.increments("id")
+        .unsigned()
+        .primary();
+      t.string("text").notNull();
+      t.integer("question")
+        .references("id")
+        .inTable("question")
+        .notNull()
+        .onDelete("cascade");
+      t.integer("author")
+        .references("id")
+        .inTable("user")
+        .notNull()
+        .onDelete("cascade");
+      t.timestamps();
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTable("question")
+    .dropTable("answer")
+    .dropTable("user");
+};
